14ea5f1058ad97a759ec8a408f78dee6916b1d5d
Skyler Suzuki <skylersuzuk1@gmail.com>
2020-02-19 14:32:39 -0800

    Added uicolor+extension file to help with hex color codes and created a structure to hold all the colors associated with mneme. Changed colors in tab bar to update.

--- mNeme/mNeme/TabViewController.swift
+++ mNeme/mNeme/TabViewController.swift
@@ -11,6 +11,11 @@ import SOTabBar
 
 class TabViewController: SOTabBarController {
 
+    override func loadView() {
+        super.loadView()
+        SOTabBarSetting.tabBarTintColor = UIColor.mNeme.orangeBlaze
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
         setUpTabViewControllers()
@@ -18,19 +23,27 @@ class TabViewController: SOTabBarController {
     }
 
     private func setUpTabViewControllers() {
+        // instantiating each navcontroller for view controller on tab bar
         let homeVC = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "HomeNavController")
 
         let deckCreateVC = UIStoryboard(name: "Main", bundle:nil).instantiateViewController(withIdentifier: "DeckCreateNavController")
 
         let profileVC = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "ProfileNavController")
 
-        let homeImage = UIImage(systemName: "house")
-        let createImage = UIImage(systemName: "pencil")
-        let profileImage = UIImage(systemName: "person")
+        // Constants for tab bar icons/selected
+        let homeImage = UIImage(systemName: "house")?.withTintColor(UIColor.mNeme.orangeBlaze, renderingMode: .alwaysOriginal)
+        let homeImageSelected = UIImage(systemName: "house")?.withTintColor(UIColor.white, renderingMode: .alwaysOriginal)
 
-        homeVC.tabBarItem = UITabBarItem(title: "Home", image: homeImage, selectedImage: homeImage)
-        deckCreateVC.tabBarItem = UITabBarItem(title: "Create Deck", image: createImage, selectedImage: createImage)
-        profileVC.tabBarItem = UITabBarItem(title: "Profile", image: profileImage, selectedImage: profileImage)
+        let createImage = UIImage(systemName: "pencil")?.withTintColor(UIColor.mNeme.orangeBlaze, renderingMode: .alwaysOriginal)
+        let createImageSelected = UIImage(systemName: "pencil")?.withTintColor(UIColor.white, renderingMode: .alwaysOriginal)
+
+        let profileImage = UIImage(systemName: "person")?.withTintColor(UIColor.mNeme.orangeBlaze, renderingMode: .alwaysOriginal)
+        let profileImageSelected = UIImage(systemName: "person")?.withTintColor(UIColor.white, renderingMode: .alwaysOriginal)
+
+        // Setting up tab bar items for each view controller
+        homeVC.tabBarItem = UITabBarItem(title: "Home", image: homeImage, selectedImage: homeImageSelected)
+        deckCreateVC.tabBarItem = UITabBarItem(title: "Create Deck", image: createImage, selectedImage: createImageSelected)
+        profileVC.tabBarItem = UITabBarItem(title: "Profile", image: profileImage, selectedImage: profileImageSelected)
 
         viewControllers = [homeVC, deckCreateVC, profileVC]
     }
