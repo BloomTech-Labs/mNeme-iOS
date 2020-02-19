34d8b411aae0f05864b9bef7f0f9d6fdb6f67d55
Nar Kumar <njn.kumar@gmail.com>
2020-02-19 15:12:05 -0800

    Login VC Set up - FB & Google Login working. Pair programming w/ Dennis. Storyboard Login Basic Set up

--- mNeme/mNeme/LoginViewController.swift
+++ mNeme/mNeme/LoginViewController.swift
@@ -9,19 +9,27 @@
 import UIKit
 import FirebaseAuth
 import GoogleSignIn
+import FacebookLogin
 
 class LoginViewController: UIViewController, GIDSignInDelegate {
+    
+    @IBOutlet weak var googleSignButton: GIDSignInButton!
+    @IBOutlet weak var facebookLoginButton: FBLoginButton!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         GIDSignIn.sharedInstance()?.presentingViewController = self
         GIDSignIn.sharedInstance().delegate = self
         
+        facebookLoginButton.delegate = self
+        
     }
     
     @IBAction func googleSignInPressed(_ sender: Any) {
-         GIDSignIn.sharedInstance().signIn()
+        GIDSignIn.sharedInstance().signIn()
     }
+    
     
     func sign(_ signIn: GIDSignIn!, didSignInFor user: GIDGoogleUser!, withError error: Error!) {
         if let error = error {
@@ -35,20 +43,45 @@ class LoginViewController: UIViewController, GIDSignInDelegate {
                 print(error.localizedDescription)
             } else {
                 print("Login Successful.")
-//                self.performSegue(withIdentifier: "LoginSegue", sender: self)
+                //                self.performSegue(withIdentifier: "LoginSegue", sender: self)
             }
         }
     }
     
-
+    
     /*
-    // MARK: - Navigation
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
+}
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+extension LoginViewController: LoginButtonDelegate {
+    func loginButton(_ loginButton: FBLoginButton, didCompleteWith result: LoginManagerLoginResult?, error: Error?) {
+        if let error = error {
+            print(error.localizedDescription)
+            return
+        }
+        
+        let credential = FacebookAuthProvider.credential(withAccessToken: AccessToken.current!.tokenString)
+        Auth.auth().signIn(with: credential) { (authResult, error) in
+            if let error = error {
+                print(error.localizedDescription)
+                return
+            } else {
+                print("Login Successful")
+            }
+        }
     }
-    */
-
+    
+    func loginButtonDidLogOut(_ loginButton: FBLoginButton) {
+        
+    }
+    
+    
 }
--- mNeme/mNeme/Resources/AppDelegate.swift
+++ mNeme/mNeme/Resources/AppDelegate.swift
@@ -15,7 +15,6 @@ import GoogleSignIn
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
         FirebaseApp.configure()
