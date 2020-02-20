80a2eb7e1a0e3425065e9ead1e1571b97c117d75
Skyler Suzuki <skylersuzuk1@gmail.com>
2020-02-20 14:31:27 -0800

    Added ids for each button to access them when toggling them through clicking on them. Set up functions to take care of each one.

--- mNeme/mNeme/ProfileViewController.swift
+++ mNeme/mNeme/ProfileViewController.swift
@@ -19,6 +19,7 @@ class ProfileViewController: UIViewController {
     @IBOutlet private weak var notificationButton: UIButton!
     @IBOutlet private weak var saveButton: UIButton!
 
+    // MARK: Views
     override func viewDidLoad() {
         super.viewDidLoad()
         updateViews()
@@ -26,12 +27,77 @@ class ProfileViewController: UIViewController {
     }
 
     private func updateViews() {
-        view.backgroundColor = UIColor.mNeme.orangeBlaze
+        //view.backgroundColor = UIColor.mNeme.orangeBlaze
         buttonViews()
     }
 
     private func buttonViews() {
+        mobileButton.accessibilityIdentifier = "mobileButtonID"
+        desktopButton.accessibilityIdentifier = "desktopButtonID"
+        preMadeDeckButton.accessibilityIdentifier = "preMadeDeckButtonID"
+        customDeckButton.accessibilityIdentifier = "customDeckButtonID"
+    }
 
+    // MARK: IBActions
+    @IBAction private func mobileButtonTapped(_ sender: UIButton) {
+        studyDevicePreferences(sender)
+    }
+
+    @IBAction private func desktopButtonTapped(_ sender: UIButton) {
+        studyDevicePreferences(sender)
+    }
+
+    @IBAction private func preMadeDeckButtonTapped(_ sender: UIButton) {
+        flashcardDeckPreferences(sender)
+    }
+
+    @IBAction private func customDeckButtonTapped(_ sender: UIButton) {
+        flashcardDeckPreferences(sender)
+    }
+
+    // MARK: Private methods
+    private func studyDevicePreferences(_ sender: UIButton) {
+        if !mobileButton.isSelected && !desktopButton.isSelected {
+            if sender.accessibilityIdentifier == "mobileButtonID" {
+                mobileButton.isSelected.toggle()
+            } else if sender.accessibilityIdentifier == "desktopButtonID" {
+                desktopButton.isSelected.toggle()
+            }
+        }
+
+        if sender.accessibilityIdentifier == "desktopButtonID" {
+            if !desktopButton.isSelected {
+                desktopButton.isSelected.toggle()
+                mobileButton.isSelected.toggle()
+            }
+        } else if sender.accessibilityIdentifier == "mobileButtonID" {
+            if !mobileButton.isSelected {
+                mobileButton.isSelected.toggle()
+                desktopButton.isSelected.toggle()
+            }
+        }
+    }
+
+    private func flashcardDeckPreferences(_ sender: UIButton) {
+        if !preMadeDeckButton.isSelected && !customDeckButton.isSelected {
+            if sender.accessibilityIdentifier == "preMadeDeckButtonID" {
+                preMadeDeckButton.isSelected.toggle()
+            } else if sender.accessibilityIdentifier == "customDeckButtonID" {
+                customDeckButton.isSelected.toggle()
+            }
+        }
+
+        if sender.accessibilityIdentifier == "preMadeDeckButtonID" {
+            if !preMadeDeckButton.isSelected {
+                preMadeDeckButton.isSelected.toggle()
+                customDeckButton.isSelected.toggle()
+            }
+        } else if sender.accessibilityIdentifier == "customDeckButtonID" {
+            if !customDeckButton.isSelected {
+                preMadeDeckButton.isSelected.toggle()
+                customDeckButton.isSelected.toggle()
+            }
+        }
     }
     /*
     // MARK: - Navigation
