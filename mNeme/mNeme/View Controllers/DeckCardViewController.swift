2acdf11b9676ddbdfc87bdc92b5603f6d1222538
Dennis Rudolph <Dennis@Denniss-MacBook-Pro.local>
2020-02-24 20:03:58 -0500

    Added a card flipping animation to display front and back sides of card / pp with Nar

--- mNeme/mNeme/View Controllers/DeckCardViewController.swift
+++ mNeme/mNeme/View Controllers/DeckCardViewController.swift
@@ -12,31 +12,47 @@ class DeckCardViewController: UIViewController {
     
     @IBOutlet weak var containerView: UIView!
     
-    private let frontLabel: UILabel! = UILabel()
-    private let backLabel: UILabel! = UILabel()
+    private var frontLabel: UILabel!
+    private var backLabel: UILabel!
     
-    private var showingFront = true
+//    var cards: [Card]? = []
+//    var currentCard: Card?
+    var currentCardIndex: Int = 1
+    
+    private var showingBack = false
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        frontLabel.contentMode = .scaleAspectFit
-        backLabel.contentMode = .scaleAspectFit
-
-        containerView.addSubview(frontLabel)
         
-//        let singleTap = UITapGestureRecognizer(target: self, action: #selector(flip))
-//        singleTap.numberOfTapsRequired = 1
-//        containerView.addGestureRecognizer(singleTap)
-//    }
-//
-//    func flip() {
-//        let toView = showingBack ? frontImageView : backImageView
-//        let fromView = showingBack ? backImageView : frontImageView
-//        UIView.transitionFromView(fromView, toView: toView, duration: 1, options: .TransitionFlipFromRight, completion: nil)
-//        toView.translatesAutoresizingMaskIntoConstraints = false
-//        toView.spanSuperview()
-//        showingBack = !showingBack
+        frontLabel = UILabel(frame: CGRect(x: self.containerView.frame.width/2, y: self.containerView.frame.height/2, width: 80, height: 50))
+        backLabel = UILabel(frame: CGRect(x: self.containerView.frame.width/2, y: self.containerView.frame.height/2, width: 80, height: 50))
+        
+//        cards[currentCardIndex]
+        frontLabel?.text = "front"
+        backLabel?.text = "back"
+        backLabel?.isHidden = true
+//        frontLabel.contentMode = .scaleAspectFit
+//        backLabel.contentMode = .scaleAspectFit
+
+        containerView.addSubview(frontLabel!)
+        containerView.addSubview(backLabel!)
+        
+        let singleTap = UITapGestureRecognizer(target: self, action: #selector(flip))
+        singleTap.numberOfTapsRequired = 1
+        containerView.addGestureRecognizer(singleTap)
+    }
+
+    @objc func flip() {
+        backLabel?.isHidden.toggle()
+        let toView = showingBack ? frontLabel : backLabel
+        let fromView = showingBack ? backLabel : frontLabel
+        UIView.transition(from: fromView!, to: toView!, duration: 1, options: .transitionFlipFromRight, completion: nil)
+        toView?.translatesAutoresizingMaskIntoConstraints = false
+        showingBack = !showingBack
+    }
+    
+    private func setCurrentCard() {
+        
     }
     /*
     // MARK: - Navigation
