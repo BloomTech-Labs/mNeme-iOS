fc869d8951792b2751ee91b29bc943c8c4bbd431
Nar Kumar <njn.kumar@gmail.com>
2020-02-25 12:42:51 -0800

    Mock JSON Deck Data imported. Working on Flipping through Mock Demo Deck with card animation. Unit Test working. Pair progarmming w/ Dennis.

---
+++ mNeme/mNeme/Models/MockDeck.swift
@@ -0,0 +1,129 @@
+//
+//  MockDeck.swift
+//  mNeme
+//
+//  Created by Niranjan Kumar on 2/25/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import Foundation
+
+
+struct MockDemoDeck: Codable {
+    var deckName: String
+    var data: [CardData]
+    
+}
+
+struct CardData: Codable {
+    var id: String
+    var data: CardInfo
+}
+
+struct CardInfo: Codable {
+    var back, front: String
+}
+
+class MockDemoDeckController {
+    
+    var deck: MockDemoDeck?
+    
+    let deckData = """
+    {
+      "deckName": "mNeme",
+      "data": [
+        {
+          "id": "9qzYSx8CK7AgLaOAgZ4a",
+          "data": {
+            "back": "Greg Johnston,  Tyler Thompson, William Berlin, Rayven Burns,  Ndawi Eke, Patrick Chow, Sarina Garg, Kyla Sweeney,",
+            "front": "mNeme Team"
+          }
+        },
+        {
+          "id": "DUXAdcb2lM2US70hJWeE",
+          "data": {
+            "back": "Spaced repetition is an evidence-based learning technique that is usually performed with flashcards",
+            "front": "Spaced Repetition"
+          }
+        },
+        {
+          "id": "K6iATp6sFPPF7Jl6VGR1",
+          "data": {
+            "front": "Flashcard",
+            "back": "a card containing a small amount of information; an aid to learning."
+          }
+        },
+        {
+          "id": "fJH6rn46Pfm7srNg1fSp",
+          "data": {
+            "back": "A widely used method of efficiently using flashcards that was proposed by the German science journalist Sebastian Leitner in the 1970s. It is a simple implementation of the principle of spaced repetition, where cards are reviewed at increasing intervals.",
+            "front": "Leitner System"
+          }
+        },
+        {
+          "id": "jg2H8vAvjpWrBCYrlvl5",
+          "data": {
+            "back": "comprehensive knowledge or skill in a subject or accomplishment",
+            "front": "Self-Rating"
+          }
+        },
+        {
+          "id": "nhedkm0mwE21gtd0eyGn",
+          "data": {
+            "back": "The act of thinking about how well you know given content",
+            "front": "Metacognition"
+          }
+        },
+        {
+          "id": "p8MObalaiZLBS1MZw2Oo",
+          "data": {
+            "back": "Attempting to remember a concept from scratch",
+            "front": "Active Recall"
+          }
+        },
+        {
+          "id": "q7plyvCoe4dK7BqF3TgL",
+          "data": {
+            "back": "a small digital image or icon used to express an idea, emotion, etc.",
+            "front": "Emoji"
+          }
+        },
+        {
+          "id": "wRAXBGC66Si1UUHjjVr4",
+          "data": {
+            "back": "In Greek mythology, mNeme /ˈniːmi/ (Greek: Μνήμη Mnḗmē) was one of the three original (Boeotian) muses. mNeme is the muse of memory",
+            "front": "mNeme"
+          }
+        },
+        {
+          "id": "xJIUd7La13CI78XgSpKg",
+          "data": {
+            "front": "Mastery",
+            "back": "comprehensive knowledge or skill in a subject or accomplishment"
+          }
+        }
+      ]
+    }
+    """.data(using: .utf8)!
+    
+    let dataLoader: NetworkDataLoader
+    
+    init(networkDataLoader: NetworkDataLoader = URLSession.shared) {
+        self.dataLoader = networkDataLoader
+    }
+    
+    
+    func decodeMockData() -> MockDemoDeck? {
+        //        dataLoader.loadData(using: URLRequest(url: URL(string: "")!)) {data,_,_ in
+        let decoder = JSONDecoder()
+        do {
+            let decodedMockData = try decoder.decode(MockDemoDeck.self, from: self.deckData)
+            return decodedMockData
+        } catch {
+            NSLog("Did not work decoding mock data deck")
+            return nil
+        }
+        //        }
+    }
+    
+}
--- mNeme/mNeme/View Controllers/DeckCardViewController.swift
+++ mNeme/mNeme/View Controllers/DeckCardViewController.swift
@@ -10,40 +10,61 @@ import UIKit
 
 class DeckCardViewController: UIViewController {
     
+    // MARK: - Outlets
     @IBOutlet weak var containerView: UIView!
+    @IBOutlet weak var badRating: UIButton!
+    @IBOutlet weak var okayRating: UIButton!
+    @IBOutlet weak var greatRating: UIButton!
+    @IBOutlet weak var nextCardButton: UIButton!
+    @IBOutlet weak var wellKnownQuestion: UILabel!
     
     private var frontLabel: UILabel!
     private var backLabel: UILabel!
     
-//    var cards: [Card]? = []
-//    var currentCard: Card?
-    var currentCardIndex: Int = 1
+    var deck: MockDemoDeck?
+    var cards: [CardData] = []
+    var currentCardInfo: CardInfo?
+    var currentCardIndex: Int = 0
+    
+    var mockDemoDeckController = MockDemoDeckController()
     
     private var showingBack = false
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        frontLabel = UILabel(frame: CGRect(x: self.containerView.frame.width/2, y: self.containerView.frame.height/2, width: 80, height: 50))
-        backLabel = UILabel(frame: CGRect(x: self.containerView.frame.width/2, y: self.containerView.frame.height/2, width: 80, height: 50))
-        
-//        cards[currentCardIndex]
-        frontLabel?.text = "front"
-        backLabel?.text = "back"
-        backLabel?.isHidden = true
-//        frontLabel.contentMode = .scaleAspectFit
-//        backLabel.contentMode = .scaleAspectFit
+        updateViews()
+        testDemoDeck()
 
-        containerView.addSubview(frontLabel!)
-        containerView.addSubview(backLabel!)
-        
         let singleTap = UITapGestureRecognizer(target: self, action: #selector(flip))
         singleTap.numberOfTapsRequired = 1
         containerView.addGestureRecognizer(singleTap)
     }
+    
+    // MARK: - IB Actions
+
+    @IBAction func badRatingTapped(_ sender: Any) {
+        
+    }
+    
+    @IBAction func okayRatingTapped(_ sender: Any) {
+    }
+    
+    @IBAction func greatRatingTapped(_ sender: Any) {
+        
+    }
+    
+    @IBAction func nextCardButtonTapped(_ sender: Any) {
+        wellKnownQuestion?.isHidden.toggle()
+    }
 
     @objc func flip() {
         backLabel?.isHidden.toggle()
+        wellKnownQuestion?.isHidden.toggle()
+        badRating?.isHidden.toggle()
+        okayRating?.isHidden.toggle()
+        greatRating?.isHidden.toggle()
+        
         let toView = showingBack ? frontLabel : backLabel
         let fromView = showingBack ? backLabel : frontLabel
         UIView.transition(from: fromView!, to: toView!, duration: 1, options: .transitionFlipFromRight, completion: nil)
@@ -51,9 +72,32 @@ class DeckCardViewController: UIViewController {
         showingBack = !showingBack
     }
     
-    private func setCurrentCard() {
+    private func testDemoDeck() {
+        deck = mockDemoDeckController.decodeMockData()
+        
+        var currentCardInfo = deck?.data[currentCardIndex].data
+        
+        frontLabel?.text = currentCardInfo?.front
+        backLabel?.text = currentCardInfo?.back
+            
+    }
+    
+    private func updateViews() {
+        frontLabel = UILabel(frame: CGRect(x: self.containerView.frame.width/2, y: self.containerView.frame.height/2, width: 80, height: 50))
+        backLabel = UILabel(frame: CGRect(x: self.containerView.frame.width/2, y: self.containerView.frame.height/2, width: 80, height: 50))
+
+//        backLabel?.isHidden = true
+//        nextCardButton?.isHidden = true
+//        wellKnownQuestion?.isHidden = true
+//        badRating?.isHidden = true
+//        okayRating?.isHidden = true
+//        greatRating?.isHidden = true
+
+        containerView.addSubview(frontLabel!)
+        containerView.addSubview(backLabel!)
         
     }
+    
     /*
     // MARK: - Navigation
 
