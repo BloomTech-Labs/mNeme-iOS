8c3cc33222724b4bfb5fdd3e7e0504312591f347
Nar Kumar <njn.kumar@gmail.com>
2020-03-13 16:44:33 -0700

    Rewired and refactored storyboards. Deleted scroll view, and moved everything into CreateDeckVC. Moved and refactored code into CreateDeckVC. Need to fix sections and cells inside Tableview for adding card and viewing a card.

---
+++ mNeme/mNeme/View Controllers/CreateCardTableViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  CreateCardTableViewCell.swift
+//  mNeme
+//
+//  Created by Niranjan Kumar on 3/13/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class CreateCardTableViewCell: UITableViewCell {
+
+
+
+}
