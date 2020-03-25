14fa6b9e7bd309bd9c60f7d513f4075214c0b189
Dennis Rudolph <Dennis@Denniss-MacBook-Pro.local>
2020-03-25 12:02:45 -0400

    Fixed Google/FB login bug, added no encryption to plist / pp with Nar and Skyler

--- mNeme/mNeme/Model Controllers/DeckController.swift
+++ mNeme/mNeme/Model Controllers/DeckController.swift
@@ -130,7 +130,7 @@ class DeckController {
                         print(result)
                         completion()
                     }
-                }
+                } else {completion()}
             }
         }
     }
